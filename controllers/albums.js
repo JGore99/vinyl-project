1df30490835596cf22dc9ab31faa062e45a21e22
Jeff_Gore <JefGore@gmail.com>
2021-11-08 12:39:41 -0500

    user details added

--- controllers/albums.js
+++ controllers/albums.js
@@ -16,13 +16,16 @@ function search(req, res) {
 }
 
 function show(req, res) {
+  console.log("REQ PARAMS----------", req.params.id)
   axios.get(`https://api.discogs.com/database/search?type=release&q=${req.params.id}&token=${process.env.TOKEN}`)
   .then(response => {
-    Album.findOne({ discogsId: response.data.results.id })
+    Album.findOne({ discogsId: response.data.id })
+    .populate("collectedBy")
     .then((album) => {
+      console.log("discogsId----------", response.data.id)
       res.render("albums/show", {
         title: "Album Details",
-        apiResult: response.data.results,
+        apiResult: response.data,
         album,
         userHasAlbum: album?.collectedBy.some(profile => profile._id.equals(req.user.profile._id)),
       })
